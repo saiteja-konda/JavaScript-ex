728bab8d0b9417ae31c1ad48f999ef028d7dd0b1
saiteja <kondasaitej@protonmail.com>
2020-08-18 13:18:55 +0530

    fun&cc

---
+++ src/components/greet.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Greet = () => <h1>This is functional component</h1>;
+
+export default Greet;
---
+++ src/components/welcome.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class Welcome extends Component {
+  state = {};
+  render() {
+    return <h1>This is class component</h1>;
+  }
+}
+
+export default Welcome;
