0d9bc1ca0b5ca0834fb7408721214cd656dd6cd2
saiteja <kondasaitej@protonmail.com>
2020-09-19 01:10:17 +0530

    useState 2

--- src/components/33 Hooks/hooks.jsx
+++ src/components/33 Hooks/hooks.jsx
@@ -1,13 +1,22 @@
 import React, { Component, useState } from "react";
-function Hooks() {
-  const [Text, setText] = useState("default");
+const Hooks = () => {
+  //   const [Text, setText] = useState("");
+  const [fruit, setFruit] = useState("");
+  const [vegitable, setVegitable] = useState("");
 
   return (
     <div>
-      <h1>{Text}</h1>
-      <input value={Text} onChange={(e) => setText(e.target.value)} />
+      <lable>Fruit Name </lable>
+      <input value={fruit} onChange={(e) => setFruit(e.target.value)} />
+      <br /> <br />
+      <lable>Vegitable Name </lable>
+      <input value={vegitable} onChange={(e) => setVegitable(e.target.value)} />
+      <br />
+      <br />
+      <br />
+      My fav fruit is <b>{fruit}</b> and my fav vegitable is <b>{vegitable}</b>
     </div>
   );
-}
+};
 
 export default Hooks;
