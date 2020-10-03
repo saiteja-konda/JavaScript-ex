c01f7f56249ba2fa64693370d77602dad2328e50
saiteja <kondasaitej@protonmail.com>
2020-10-03 14:23:37 +0530

    Redux
    Create read and delete posts effectively with global state

--- my javaScript/Redux-Demo/AsyncActions.js
+++ my javaScript/Redux-Demo/AsyncActions.js
@@ -2,7 +2,10 @@ const redux = require('redux');
 const createStore = redux.createStore;
 const applyMiddleware = redux.applyMiddleware;
 const thunkMiddleware = require('redux-thunk').default;
+const reduxLogger = require('redux-logger');
 const axios = require('axios');
+const logger = reduxLogger.createLogger();
+const url = 'http://localhost:8080/post';
 
 //State
 const initialState = {
@@ -16,25 +19,87 @@ const FETCH_POSTS_REQUEST = 'FETCH_POSTS_REQUEST';
 const FETCH_POSTS_SUCCESS = 'FETCH_POSTS_SUCCESS';
 const FETCH_POSTS_FAILURE = 'FETCH_POSTS_FAILURE';
 
+const ADD_POST_REQUEST = 'ADD_POST_REQUEST';
+const ADD_POST_SUCCESS = 'ADD_POST_SUCCESS';
+const ADD_POST_FAILURE = 'ADD_POST_FAILURE';
+
+const DELETE_POST_REQUEST = 'DELETE_POST_REQUEST';
+const DELETE_POST_SUCCESS = 'DELETE_POST_SUCCESS';
+const DELETE_POST_FAILURE = 'DELETE_POST_FAILURE';
+
 const fetchPostRequest = () => {
   return {
     type: FETCH_POSTS_REQUEST,
-    info: 'Requesting Fetch Users',
+    info: 'Requesting Fetch posts',
+    status: 'orange',
   };
 };
 
-const fetchPostSuccess = users => {
+const fetchPostSuccess = posts => {
   return {
     type: FETCH_POSTS_SUCCESS,
-    info: 'Fetched Users Successfully',
-    payload: users,
+    info: 'Fetched posts Successfully',
+    payload: posts,
+    status: 'green',
   };
 };
 
 const fetchPostFailure = error => {
   return {
     type: FETCH_POSTS_FAILURE,
-    info: 'Fetched Users Failed',
+    info: 'Fetched posts Failed',
+    payload: error,
+    status: 'red',
+  };
+};
+
+const addNewPostRequest = () => {
+  return {
+    type: ADD_POST_REQUEST,
+    info: ' Requesting to add new post',
+    status: 'orange',
+  };
+};
+
+const addNewPostSuccess = post => {
+  return {
+    type: ADD_POST_SUCCESS,
+    info: 'Post added Successfully',
+    status: 'green',
+    payload: {
+      posts: post,
+    },
+  };
+};
+const addNewPostFailure = error => {
+  return {
+    type: ADD_POST_FAILURE,
+    info: 'Failed to add Post',
+    status: 'red',
+    payload: error,
+  };
+};
+
+const deletePostRequest = () => {
+  return {
+    type: DELETE_POST_REQUEST,
+    info: 'Requesting to delete post',
+    status: 'orange',
+  };
+};
+const deletePostSuccess = id => {
+  return {
+    type: DELETE_POST_SUCCESS,
+    info: 'Deleted post Successfully',
+    status: 'green',
+    payload: id,
+  };
+};
+const deletePostFailure = error => {
+  return {
+    type: DELETE_POST_FAILURE,
+    info: 'Failed to delete post',
+    status: 'red',
     payload: error,
   };
 };
@@ -58,6 +123,42 @@ const postReducer = (state = initialState, action) => {
         loading: false,
         error: action.payload,
       };
+    case ADD_POST_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case ADD_POST_SUCCESS:
+      return {
+        posts: [...state.posts, action.payload.posts],
+        loading: false,
+      };
+
+    case ADD_POST_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case DELETE_POST_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case DELETE_POST_SUCCESS:
+      return {
+         ...state,
+        posts:  state.posts.filter(
+            post => post.id !== action.payload
+            ),
+        loading: false,
+      };
+    case DELETE_POST_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
     default:
       return initialState;
   }
@@ -67,7 +168,7 @@ const fetchPosts = () => {
     dispatch(fetchPostRequest());
 
     axios
-      .get('http://localhost:8080/post/type/trending')
+      .get(url)
       .then(response => {
         const posts = response.data.map(post => post);
         dispatch(fetchPostSuccess(posts));
@@ -77,8 +178,45 @@ const fetchPosts = () => {
       });
   };
 };
-const store = createStore(postReducer, applyMiddleware(thunkMiddleware));
-store.subscribe(() => {
-  console.log(store.getState());
-});
+
+const addPost = post => {
+  return function (dispatch) {
+    dispatch(addNewPostRequest());
+    axios
+      .post(url, post)
+      .then(res => {
+        dispatch(addNewPostSuccess(res.data));
+      })
+      .catch(error => {
+        dispatch(addNewPostFailure(error.response.data));
+      });
+  };
+};
+
+const deletePost = id => {
+  return function (dispatch) {
+    dispatch(deletePostRequest());
+    axios
+      .delete(`http://localhost:8080/post/${id}`)
+      .then(res => {
+        // console.log(res.response)
+        dispatch(deletePostSuccess(id));
+      })
+      .catch(err => {
+        dispatch(deletePostFailure(err.response.data));
+      });
+  };
+};
+const store = createStore(postReducer, applyMiddleware(thunkMiddleware, logger));
+store.subscribe(() => {});
+
+const newPost = {
+  title: ' Work load 2',
+  description: ';knojn',
+  content: 'ljhouhnuon',
+  type: 'test',
+  categoryId: '1',
+};
 store.dispatch(fetchPosts());
+store.dispatch(addPost(newPost));
+store.dispatch(deletePost(60));
